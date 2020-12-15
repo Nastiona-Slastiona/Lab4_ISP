0985f87583095175c00eb5cd041ff00bf96645de
Nastiona-Slastiona <ariaignis.dragon@gmail.com>
2020-12-15 17:23:00 +0300

    Add files via upload

---
+++ DB_Models/SearchModels/SearchCriteria.cs
@@ -0,0 +1,9 @@
+﻿namespace DB_Models.SearchModels
+{
+    public class SearchCriteria
+    {
+        public int Page { get; set; }
+        public int Count { get; set; }
+        public int ID { get; set; }
+    }
+}

7dabf49a5104b1f2c4f165650d267052b673af80
Nastiona-Slastiona <ariaignis.dragon@gmail.com>
2020-12-15 17:24:52 +0300

    Add files via upload

---
+++ DB_ServiceLayer/Generating/IXMLGeneratorService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DB_ServiceLayer.Generating
+{
+    public interface IGenerator<in TEntity>
+    {
+        void XML_Generating();
+
+        void XSD_Generating();
+    }
+}

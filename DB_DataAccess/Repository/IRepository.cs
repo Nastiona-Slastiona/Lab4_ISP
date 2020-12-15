bdc3407cc010b6aa50c15d04e03f3ae2290adb68
Nastiona-Slastiona <ariaignis.dragon@gmail.com>
2020-12-15 17:18:54 +0300

    Add files via upload

---
+++ DB_DataAccess/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using DB_Models;
+using DB_Models.SearchModels;
+using System.Collections.Generic;
+
+namespace DB_DataAccess.Repository
+{
+    public interface IRepository<TEntity> where TEntity: class
+    {
+        IEnumerable<TEntity> GetALLInf(SearchCriteria searchCriteria);
+        TEntity GetEntity(SearchCriteria searchCriteria);
+
+
+    }
+}
